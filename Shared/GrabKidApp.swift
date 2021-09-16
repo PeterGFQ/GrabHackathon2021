3aaefeb82f86898f9c5259f5457b3ffb6410805e
Peter Guo <peter.guo@grabtaxi.com>
2021-09-17 01:53:26 +0800

    Initial Commit

---
+++ Shared/GrabKidApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrabKidApp.swift
+//  Shared
+//
+//  Created by Peter Guo on 17/9/21.
+//
+
+import SwiftUI
+
+@main
+struct GrabKidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
